92000cf4cdb42b72f92d2d6ec1701a4b55a2a32e
user <user@host.user>
2016-06-19 01:49:44 +0000

    Initial import

---
+++ config.py
@@ -0,0 +1,89 @@
+"""Common functions for OII Agents."""
+# urljoin only join with one argument
+# from urlparse import urljoin
+from os.path import join, abspath, dirname
+
+
+BASE_PATH = abspath(__file__)
+# BASE_PATH = abspath('.')
+ROOT_PATH = dirname(BASE_PATH)
+PROJECT_PATH = dirname(ROOT_PATH)
+ROOT_PROJECT_PATH = dirname(PROJECT_PATH)
+# in case agents-common-code is not installed, the path to it is requered
+AGENTS_MODULE_DIR = 'agents-common-code'
+AGENTS_MODULE_PATH = join(PROJECT_PATH, AGENTS_MODULE_DIR)
+
+# FIXME: the ide should be generated other way
+AGENT_ID = 'agent-tos-1'
+
+# configuration yaml file. All the configuration here could go there
+CONFIG = join(ROOT_PATH, 'config.yaml')
+
+# interval of time in which the agent is going to retrieve its configuration
+INTERVAL = 10
+
+# couchdb configuration and urls
+COUCHDB_URL = 'https://oii-db.iilab.org'
+TOS_CONFIG_DB = 'staging-config'
+TOS_CONFIG_DOC = 'tos'
+# TOS_CONFIG_URL = 'https://oii-db.iilab.org/staging-config/tos'
+TOS_CONFIG_URL = '/'.join([COUCHDB_URL, TOS_CONFIG_DB, TOS_CONFIG_DOC])
+TOS_ETAG_URL = """https://oii-db.iilab.org/staging-tos/_design/couchapp-tos/_view/tos?reduce=true&group_level=2&startkey=["%s"]&endkey=["%s",{}]"""
+# TOS_ETAG_POST_URL = "https://oii-db.iilab.org/staging-tos/tos-1"
+
+TOS_ETAG_DB = 'staging-tos'
+# FIXME: should be this a hash of the agent and the url?
+# TOS_ETAG_DOC = 'tos-1'
+# TOS_ETAG_POST_URL = '/'.join([COUCHDB_URL, TOS_ETAG_DB, TOS_ETAG_DOC])
+TOS_ETAG_DOC = AGENT_ID + '-%s'
+TOS_ETAG_PART_POST_URL = '/'.join([COUCHDB_URL, TOS_ETAG_DB]) + '/%s' 
+
+# fetch_url configuration
+# FIXME: temporal url for development
+FETCH_URL = 'http://127.0.0.1:8000/fetchurl/%s/%s/%s'
+
+# rabbitmq configuration
+AMQP_CONFIG = {'AMQP_URI': 'amqp://guest:guest@localhost'}
+
+# logging configuration
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'simple': {
+            'format': "%(levelname)s:%(name)s - %(module)s - %(message)s"
+        }
+    },
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler',
+            'include_html': True,
+        },
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'nameko': {
+            'level': 'DEBUG',
+            'handlers': ['console']
+        }
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['console']
+    }
+}
---
+++ watch_url.py
@@ -0,0 +1,93 @@
+"""watch_url."""
+# from nameko.rpc import rpc, RpcProxy
+from nameko.timer import timer
+from nameko.runners import ServiceRunner
+from nameko.testing.utils import get_container
+import yaml
+import logging
+import logging.config
+# from logger import LoggingDependency
+from config import CONFIG, AMQP_CONFIG, INTERVAL, \
+    TOS_CONFIG_URL, TOS_ETAG_URL, TOS_ETAG_DOC, TOS_ETAG_PART_POST_URL,\
+    FETCH_URL
+from couchdb_watch import get_db_rules, get_db_etag, push_db_etag, fetch_url
+try:
+    from agents_common.etag_requests import get_etag
+except:
+    from config import AGENTS_MODULE_PATH
+    import sys
+    sys.path.append(AGENTS_MODULE_PATH)
+    from agents_common.etag_requests import get_etag
+
+
+logging.basicConfig(level=logging.DEBUG)
+with open(CONFIG) as fle:
+    config = yaml.load(fle)
+if "LOGGING" in config:
+    logging.config.dictConfig(config['LOGGING'])
+logger = logging.getLogger(__name__)
+
+class WatchURLService(object):
+    name ="watchurl"
+    # logger = LoggingDependency()
+
+    @timer(interval=INTERVAL)
+    def get_config(self):
+        rules = get_db_rules(TOS_CONFIG_URL)
+        logger.debug(rules)
+        self.watch_url(rules)
+
+    def watch_url(self, rules):
+        # for rule in rules:
+        # for development only using 1 rule
+            rule = rules[0]
+            url = rule['url']
+            # get db etag
+            # logger.debug('requesting etag in db for url %s', url)
+            # get db etag
+            etag_db, last_modified_db = get_db_etag(TOS_ETAG_URL % (url, url))
+            # get page etag
+            etag, last_modified = get_etag(url)
+            # compare etags
+            # if there weren't any etag in the database, it will be different
+            # to the one retrieved from the page and therefore it will also be
+            # stored in the database and the content fetched
+            if (etag_db != etag) or (last_modified_db != last_modified):
+                logger.info('the page has been modified')
+                # store etag in db
+                tos_etag_doc = TOS_ETAG_DOC % (rule['organization'] + '-' + rule['tool'] + '-' + rule['policy'])
+                tos_etag_post_url = TOS_ETAG_PART_POST_URL % tos_etag_doc
+                push_db_etag(tos_etag_post_url, etag, last_modified)
+                fetch_url(FETCH_URL % (url, etag, last_modified))
+            logger.info('the page has not been modified')
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+    # with open(CONFIG) as fle:
+    #     config = yaml.load(fle)
+    # if "LOGGING" in config:
+    #     logging.config.dictConfig(config['LOGGING'])
+    # # logger = logging.getLogger('nameko')
+    # # logger = logging.getLogger(__name__)
+    # logging.debug('before start')
+    with open(CONFIG) as fle:
+        config = yaml.load(fle)
+    if "LOGGING" in config:
+        logging.config.dictConfig(config['LOGGING'])
+    logger = logging.getLogger(__name__)
+    logger.debug('before start')
+
+    runner = ServiceRunner(AMQP_CONFIG)
+    runner.add_service(WatchURLService)
+    # runner.add_service(ServiceB)
+    # ``get_container`` will return the container for a particular service
+    container_watchurl = get_container(runner, WatchURLService)
+    # start both services
+    runner.start()
+    logging.debug('stop')
+    # stop both services
+    # runner.stop()
+
+
+if __name__ == '__main__':
+    main()

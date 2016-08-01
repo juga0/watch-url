73cccf2038283928b08da3217ff4e2411d368e28
juga <juga@riseup.net>
2016-08-01 17:04:57 +0000

    add console logger for stdout

--- watch_url/config_common.py
+++ watch_url/config_common.py
@@ -2,11 +2,12 @@
 # urljoin only join with one argument
 # from urlparse import urljoin
 from os.path import join, abspath, dirname
-from os import environ
+from os import environ, getpid
 
 DEBUG = environ.get('DEBUG') or False
 DATA_DIR_DEFAULT = 'data'
 LOG_FILE_DEFAULT = 'log/info.log'
+LOG_ERROR_FILE_DEFAULT = 'log/error.log'
 CONSOLE_LOG_DEFAULT = 'ERROR'
 AGENTS_MODULE_DIR = 'agents-common-code'
 STORE_URL_DEFAULT = 'https://staging-store.openintegrity.org'
@@ -57,7 +58,10 @@ AGENT_PAYLOAD = """{
 }"""
 
 # logging
-LOG_PATH = environ.get('LOG_PATH') or join(PROJECT_PATH, LOG_FILE_DEFAULT)
+LOG_PATH = environ.get('LOG_PATH') or \
+                            join(PROJECT_PATH, LOG_FILE_DEFAULT)
+LOG_ERROR_PATH = environ.get('LOG_ERROR_PATH') or \
+                            join(PROJECT_PATH, LOG_ERROR_FILE_DEFAULT)
 LOG_LEVEL = environ.get('LOG_LEVEL') or \
             ('DEBUG' if DEBUG else CONSOLE_LOG_DEFAULT)
 LOGGING = {
@@ -65,29 +69,47 @@ LOGGING = {
     'disable_existing_loggers': True,
     'formatters': {
         'simple': {
-            'format': "%(levelname)s:%(module)s - %(message)s"
+            # ERROR:watch_url_util - ...
+            'format': "%(levelname)s: %(module)s - %(message)s"
+        },
+        'detailed': {
+            'format': "%(levelname)s: %(filename)s:%(lineno)s - "
+                      "%(funcName)s - %(message)s"
+        },
+        'syslog_like': {
+            # Aug  1 11:22:43 subgraph anacron[5063]: ...
+            'format': "%(asctime)s %(name)s[%(process)d]: " \
+                      "%(levelname)s - %(message)s",
+                      # % {'host': environ.get('HOSTNAME'),
+            'datefmt': '%B %d %H:%M:%S',
         }
     },
     'handlers': {
-        'console': {
+        'console_stderr': {
             'class': 'logging.StreamHandler',
             'formatter': 'simple',
-            'level': LOG_LEVEL
+            'level': 'ERROR'
+        },
+        'console_stdout': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+            'level': LOG_LEVEL,
+            'stream': 'ext://sys.stdout'
         },
         "debug_file_handler": {
             "class": "logging.handlers.RotatingFileHandler",
             "level": "DEBUG",
-            "formatter": "simple",
+            "formatter": "syslog_like",
             "filename": LOG_PATH,
             "maxBytes": 10485760,
             "backupCount": 20,
             "encoding": "utf8"
         },
-        "info_file_handler": {
+        "error_file_handler": {
             "class": "logging.handlers.RotatingFileHandler",
             "level": "INFO",
-            "formatter": "simple",
-            "filename": LOG_PATH,
+            "formatter": "syslog_like",
+            "filename": LOG_ERROR_PATH,
             "maxBytes": 10485760,
             "backupCount": 20,
             "encoding": "utf8"
@@ -96,26 +118,25 @@ LOGGING = {
     'loggers': {
         'nameko': {
             'level': 'DEBUG',
-            'handlers': ['console', 'info_file_handler']
+            'handlers': ['console_stderr', 'debug_file_handler']
         },
         # uncomment this to get logs only from these modules and comment root
-        # FIXME: see FIXME in root
         'watch_url': {
-            # DEBUG must be here to catch all possible logs that will get filtered
-            # by the handler
+            # DEBUG must be here to catch all possible logs
+            # that will get filtered by the handler
             'level': 'DEBUG',
-            'handlers': ['console', 'debug_file_handler']
+            'handlers': ['console_stderr', 'console_stdout',
+                         'debug_file_handler']
         },
         'watch_url_util': {
             'level': 'DEBUG',
-            'handlers': ['console', 'debug_file_handler']
+            'handlers': ['console_stderr', 'console_stdout',
+                         'debug_file_handler']
         }
     },
     'root': {
-        # # DEBUG must be here to catch all possible logs that will get filtered
-        # # by the handler
-        # # FIXME: watch_url* don't log here where this uncommented
-        # # and watch_url* commented
+        # # DEBUG must be here to catch all possible logs
+        # # that will get filtered by the handler
         # 'level': 'DEBUG',
         # 'handlers': ['console', 'debug_file_handler']
     }
